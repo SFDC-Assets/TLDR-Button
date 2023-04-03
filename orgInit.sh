362ba4e6f57af0274b6ff848aa7b9af143c064cc
zhou059 <zhou059@gmail.com>
2023-04-03 16:33:58 -0700

    first take

--- orgInit.sh
+++ orgInit.sh
@@ -4,6 +4,9 @@ sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --us
 # Push the metadata into the new scratch org.
 sfdx force:source:push
 
+# Push sample data
+sfdx automig:load --inputdir ./data --deletebeforeload
+
 # Set the default password.
 sfdx shane:user:password:set -g User -l User -p salesforce1
 
